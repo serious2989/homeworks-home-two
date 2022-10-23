2d7eb8be42e0e8f7183811f95b0f78a31242866f
Dmitriy Vasilyev <serious2989@gmail.com>
2022-10-23 14:56:24 +0300

    task 47

--- Homework 20.10/Program.cs
+++ Homework 20.10/Program.cs
@@ -1,22 +1,29 @@
-﻿//Задача 43: Напишите программу, которая найдёт точку пересечения 
-//двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
-//значения b1, k1, b2 и k2 задаются пользователем.
-//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
- 
- 
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
- 
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
- 
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+﻿//Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+void FillArray(double[,] array)                                          
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round (new Random().NextDouble()*10,2);
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 3;
+int n = 4;
+double[,] array = new double [m, n];
+Console.Clear();
+FillArray(array);
+PrintArray(array);
