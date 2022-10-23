ccf14deaf2015da905b0dba083af9ca114df6866
Dmitriy Vasilev <114151384+serious2989@users.noreply.github.com>
2022-10-23 14:37:16 +0300

    Add new tasks

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+void FillArray(double[,] array)                                          
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round (new Random().NextDouble()*10,2);
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 3;
+int n = 4;
+double[,] array = new double [m, n];
+Console.Clear();
+FillArray(array);
+PrintArray(array);
